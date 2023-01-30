ac4b63f676a50ae9d2bead6d376a65b57fd0091d
Edward <edward.gratefuldead1.com>
2023-01-30 16:19:05 -0600

    Created Login

---
+++ app/src/main/java/com/heady/betterweather/view/MainActivity.java
@@ -0,0 +1,16 @@
+package com.heady.betterweather.view;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.heady.betterweather.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
